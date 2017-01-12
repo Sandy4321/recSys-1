5455cc9caf811fd1934b5f8a65f7189d461659fb
Stefano Cereda <cereda.ste@gmail.com>
2017-01-12 16:53:12 +0100

    Netflix reader can aggregate the actors

--- source/netflix_reader.py
+++ source/netflix_reader.py
@@ -1,4 +1,5 @@
 import scipy.io as sio
+import numpy as np
 
 BASEFILE = "../datasets/Enriched_Netflix_Dataset/"
 
@@ -11,3 +12,13 @@ icm_stemtypes = icm_dictionary['stemTypes'][0][0]
 titles = sio.loadmat(BASEFILE + "./titles.mat")['titles']
 
 urm = sio.loadmat(BASEFILE + "./urm.mat")['urm']
+
+
+#Now let's try to aggregate the features
+
+#The first group is composed by the actors
+actor_features_names = ['ActorsLastNameFirstArray', 'ActorsLastNameFirstArray;DirectorsLastNameFirstArray']
+actor_features = list()
+for feature_name in actor_features_names:
+    actor_features.extend(np.where(icm_stemtypes == feature_name)[0].tolist())
+#you can now obtain the relevant icm with icm_matrix[actor_features]
