1e2db45c003b674fa9029449df63065fc39f919a
Leonardo Cella <leonardocella@gmail.com>
2017-01-12 16:52:30 +0100

    minor

--- source/recSysLib/slim.py
+++ source/recSysLib/slim.py
@@ -1,4 +1,22 @@
 import numpy as np
 import scipy.sparse as sps
 from sklearn.linear_model import ElasticNet
+from .base import Recommender,check_matrix
 
+class Slim(Recommender):
+    """
+    Train a Sparse Linear Methods (SLIM) item similarity model.
+    See:
+        Efficient Top-N Recommendation by Linear Regression,
+        M. Levy and K. Jack, LSRS workshop at RecSys 2013.
+        SLIM: Sparse linear methods for top-n recommender systems,
+        X. Ning and G. Karypis, ICDM 2011.
+        http://glaros.dtc.umn.edu/gkhome/fetch/papers/SLIM2011icdm.pdf
+    """
+
+    def __init__(self, l1_penalty=0.1, l2_penalty=0.1, positive_only=True):
+        super(Slim, self).__init__()
+        self.l1_penalty = l1_penalty
+        self.l2_penalty = l2_penalty
+        self.positive_only = positive_only
+        self.l1_ratio = self.l1_penalty / (self.l1_penalty + self.l2_penalty)
