a97dedf11706a2c81bdece5f845876fdf480c1a8
Stefano Cereda <cereda.ste@gmail.com>
2017-01-12 12:02:24 +0100

    get_dataset for the new dataset

--- source/get_dataset.sh
+++ source/get_dataset.sh
@@ -2,11 +2,14 @@
 #Assuming this is launched from the source/ directory
 
 mkdir -p ../datasets/hetrec
-wget http://files.grouplens.org/datasets/hetrec2011/hetrec2011-movielens-2k-v2.zip
-mv hetrec2011-movielens-2k-v2.zip ../datasets/hetrec/
 cd ../datasets/hetrec/
+wget http://files.grouplens.org/datasets/hetrec2011/hetrec2011-movielens-2k-v2.zip
 unzip hetrec2011-movielens-2k-v2.zip
 rm hetrec2011-movielens-2k-v2.zip
 
 #convert everything to UTF-8
 ls -1 *.dat | parallel iconv -f ISO-8859-15 -t UTF-8 -o '{}' '{}'
+
+
+
+printf "%s\n" "manually download https://1drv.ms/u/s\!AtpT6fwcpsVRhLwvS0bpCrbW03v-1w and unzip to datasets/Enriched_Netflix_Dataset/"
