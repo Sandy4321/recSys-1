193f1839b2a874568092edfde54f33bb9e3672f9
Leonardo Cella <leonardocella@gmail.com>
2017-01-14 18:45:28 +0100

    minor

--- source/examples/slim_v01.py
+++ source/examples/slim_v01.py
@@ -2,16 +2,19 @@ from recSysLib.data_utils import load_sparse,save_sparse
 from netflix_reader import NetflixReader
 from recSysLib import slim
 
+TO_COMPUTE_SLIM = False
+
 # Get the URM
 
 netflixReader = NetflixReader()
 netflix_urm = netflixReader.urm
 
-
-model = slim.Slim()
-model.fit(netflix_urm,verbose=1)
-weight_matrix = model.get_weight_matrix()
-
-print("Slim-W similarity items: class type: {}, shape: {}".format(type(weight_matrix),weight_matrix.shape))
-
-save_sparse('../datasources/slim_W01',weight_matrix)
+if TO_COMPUTE_SLIM:
+    # gridSearch on l1_penalty, l2_penalty of Slim
+    model = slim.Slim()
+    model.fit(netflix_urm,verbose=1)
+    weight_matrix = model.get_weight_matrix()
+    save_sparse('../datasources/slim_W01',weight_matrix)
+else:
+    weight_matrix = load_sparse('../datasources/slim_W01.npz','csc')
+    print(weight_matrix.shape)
