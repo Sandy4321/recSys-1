9506ed813169fff0c029ffbaa544c42188f4c96a
Leonardo Cella <leonardocella@gmail.com>
2017-01-14 18:25:06 +0100

    Sparse matrix io utility functionalities

--- source/examples/slim_v01.py
+++ source/examples/slim_v01.py
@@ -1,4 +1,4 @@
-from recSysLib.data_utils import df_to_csr,read_dataset
+from recSysLib.data_utils import load_sparse,save_sparse
 from netflix_reader import NetflixReader
 from recSysLib import slim
 
@@ -13,3 +13,5 @@ model.fit(netflix_urm,verbose=1)
 weight_matrix = model.get_weight_matrix()
 
 print("Slim-W similarity items: class type: {}, shape: {}".format(type(weight_matrix),weight_matrix.shape))
+
+save_sparse('../datasources/slim_W01',weight_matrix)
--- source/recSysLib/data_utils.py
+++ source/recSysLib/data_utils.py
@@ -1,47 +1,5 @@
 import numpy as np
 import scipy.sparse as sps
-import pandas as pd
-
-
-def read_dataset(path,
-                 format='csv',
-                 header=None,
-                 columns=None,
-                 make_implicit=False,
-                 implicit_th=4.0,
-                 user_key='user_id',
-                 item_key='item_id',
-                 rating_key='rating',
-                 sep=',',
-                 user_to_idx=None,
-                 item_to_idx=None):
-
-    if format == 'csv':
-        data = pd.read_csv(path, header=header, names=columns, sep=sep)
-    elif format == 'json':
-        data = pd.read_json(path)
-
-    if make_implicit:
-        data = data[data[rating_key] >= implicit_th]   # cut-threshold for the ratings
-        data = data.reset_index(drop=True)   # reset the index to remove the 'holes' in the DataFrame
-
-    if not ('item_idx' in data.columns and 'user_idx' in data.columns):
-        # build user and item maps (and reverse maps)
-        # this is used to map ids to indexes starting from 0 to nitems (or nusers)
-        items = data[item_key].unique()
-        users = data[user_key].unique()
-        if item_to_idx is None:
-            item_to_idx = pd.Series(data=np.arange(len(items)), index=items)
-        if user_to_idx is None:
-            user_to_idx = pd.Series(data=np.arange(len(users)), index=users)
-        idx_to_item = pd.Series(index=item_to_idx.data, data=item_to_idx.index)
-        idx_to_user = pd.Series(index=user_to_idx.data, data=user_to_idx.index)
-        #  map ids to indices
-        data['item_idx'] = item_to_idx[data[item_key].values].values
-        data['user_idx'] = user_to_idx[data[user_key].values].values
-        return data, idx_to_user, idx_to_item
-    else:
-        return data
 
 
 def df_to_csr(df, nrows, ncols, is_implicit=False, user_key='user_idx', item_key='item_idx', rating_key='rating'):
@@ -56,3 +14,18 @@ def df_to_csr(df, nrows, ncols, is_implicit=False, user_key='user_idx', item_key
     # using the 4th constructor of csr_matrix
     # reference: https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.csr_matrix.html
     return sps.csr_matrix((ratings, (rows, columns)), shape=shape)
+
+def save_sparse(filename,array):
+    "Store a sparse matrix by distinguish its three components: data, indices, indptr "
+    np.savez(filename,data = array.data ,indices=array.indices,
+             indptr =array.indptr, shape=array.shape )
+
+def load_sparse(filename,type):
+    "Load a sparse matrix by distinguish its three components: data, indices, indptr"
+    loader = np.load(filename)
+    if type == 'csc':
+        return sps.csc_matrix((  loader['data'], loader['indices'], loader['indptr']),
+                         shape = loader['shape'])
+    elif type == 'csr':
+        return sps.csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
+                         shape = loader['shape'])
