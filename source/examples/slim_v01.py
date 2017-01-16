2978c731be7c4aadf72ed41d85c302998d88576d
Leonardo Cella <leonardocella@gmail.com>
2017-01-16 17:46:58 +0100

    INIT: randomicSearch slim hyperparameters

--- source/examples/slim_v01.py
+++ source/examples/slim_v01.py
@@ -1,7 +1,10 @@
 from recSysLib.netflix_reader import NetflixReader
 from recSysLib.data_split import holdout
+from recSysLib.metrics import roc_auc, precision, recall
 from recSysLib import slim
+
 import numpy as np
+from sklearn.model_selection import RandomizedSearchCV
 
 TO_COMPUTE_SLIM = True
 
@@ -10,36 +13,35 @@ TO_COMPUTE_SLIM = True
 netflixReader = NetflixReader()
 netflix_urm = netflixReader.urm
 
-print("\n===URM User Rating Matrix===")
-print("\nData Len: {}".format(len(netflix_urm.data)))
-print("Data shape: {}".format(netflix_urm.shape))
-print("Zero elements: {}".format((netflix_urm.shape[0]*netflix_urm.shape[1]) - netflix_urm.getnnz()))
-
-print("\nNon zero elements by USERS")
-print("Non zero elements all rows: {}".format(netflix_urm.getnnz(axis=1)))
-print("Non zero elements all rows len: {}".format(len(netflix_urm.getnnz(axis=1))))
-print("Non zero elements all rows sum: {}".format(np.sum(netflix_urm.getnnz(axis=1))))
-print("Non zero elements all rows avg: {}".format(np.sum(netflix_urm.getnnz(axis=1))/len(netflix_urm.getnnz(axis=1))))
-
-print("\nNon zero elements by ITEMS")
-print("Non zero elements all cols: {}".format(netflix_urm.getnnz(axis=0)))
-print("Non zero elements all cols len: {}".format(len(netflix_urm.getnnz(axis=0))))
-print("Non zero elements all cols sum: {}".format(np.sum(netflix_urm.getnnz(axis=0))))
-print("Non zero elements all cols avg: {}".format(np.sum(netflix_urm.getnnz(axis=0))/len(netflix_urm.getnnz(axis=0))))
-
-max_nnz = 0
 if TO_COMPUTE_SLIM:
-    sub_train , sub_validation = holdout(netflix_urm, perc=0.9, clean_test=True)
 
-    for l1 in [0.001 , 0.01 , 0.05 , 0.1 , 0.5]:
-        for l2 in [0.001 , 0.01 , 0.05 , 0.1 , 0.5]:
-            model = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
-            model.fit(sub_train)
-            weight_matrix = model.get_weight_matrix()
-            if weight_matrix.getnnz() > max_nnz:
-                max_nnz = weight_matrix.getnnz()
-                np.save('../datasources/slim_W01.npz',weight_matrix)
-                print("l1: {} , l2: {} , len: {}".format(l1,l2,weight_matrix.getnnz()))
+    # Split in train and validation by holdout
+    sub_train , sub_validation = holdout(netflix_urm, perc=0.95, clean_test=True)
+
+    indexes = sub_validation.nonzero()
+    print("Indexes:",indexes)
+    print("Indexes[0]",indexes[0])
+    print("Indexes[0]unique",np.unique(indexes[0]))
+
+    users_validation = np.unique(indexes[0])
+
+    for i in range(-5,2,2):
+        l1 = 10**i
+        l2 = 0
+        #l2 = numbernp.float32(random.uniform(0, 10.0))
+
+        model = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
+        model.fit(sub_train)
+        #weight_matrix = model.get_weight_matrix()
+
+        n_eval = 0
+        for user_to_test in users_validation[:3]:
+            print("User: {} \n Item indices {}".format(user_to_test,sub_validation[user_to_test].indices))
+            relevant_items = sub_validation[user_to_test].indices
+            if len(relevant_items) > 0:
+                n_eval += 1
+                recommended_items = model.recommend(user_id=user_to_test, exclude_seen=True)
+        #evaluate()
 else:
     weight_matrix = np.load('../../datasources/slim_W01.npz','csc')
 
