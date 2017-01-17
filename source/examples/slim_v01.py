093cca6a07f1b80088c81fc327fa1bada46b4389
Leonardo Cella <leonardocella@gmail.com>
2017-01-17 09:08:49 +0100

    random search slim hyp

--- source/examples/slim_v01.py
+++ source/examples/slim_v01.py
@@ -1,10 +1,9 @@
 from recSysLib.netflix_reader import NetflixReader
 from recSysLib.data_split import holdout
-from recSysLib.metrics import roc_auc, precision, recall
+from recSysLib.metrics import precision, recall
 from recSysLib import slim
 
 import numpy as np
-from sklearn.model_selection import RandomizedSearchCV
 
 TO_COMPUTE_SLIM = True
 
@@ -16,7 +15,7 @@ netflix_urm = netflixReader.urm
 if TO_COMPUTE_SLIM:
 
     # Split in train and validation by holdout
-    sub_train , sub_validation = holdout(netflix_urm, perc=0.95, clean_test=True)
+    sub_train , sub_validation = holdout(netflix_urm, perc=0.85, clean_test=True)
 
     indexes = sub_validation.nonzero()
     print("Indexes:",indexes)
@@ -25,30 +24,34 @@ if TO_COMPUTE_SLIM:
 
     users_validation = np.unique(indexes[0])
 
+    iteration = 0
     for i in range(-5,2,2):
+        iteration += 1
         l1 = 10**i
         l2 = 0
         #l2 = numbernp.float32(random.uniform(0, 10.0))
 
+        # Compute the model with the established parameters
         model = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
         model.fit(sub_train)
-        #weight_matrix = model.get_weight_matrix()
+        weight_matrix = model.get_weight_matrix()
 
+
+        # Evaluation
         n_eval = 0
-        for user_to_test in users_validation[:3]:
+        metric_ = 0.0
+        metric_1 = 0.0
+        for user_to_test in users_validation:
             print("User: {} \n Item indices {}".format(user_to_test,sub_validation[user_to_test].indices))
             relevant_items = sub_validation[user_to_test].indices
             if len(relevant_items) > 0:
                 n_eval += 1
                 recommended_items = model.recommend(user_id=user_to_test, exclude_seen=True)
-        #evaluate()
+                metric_ += precision(recommended_items, relevant_items)
+                metric_1 += recall(recommended_items, relevant_items)
+        metric_ /= n_eval
+        metric_1 /= n_eval
+
+        print("Iteration {} , l1 coeff: {}, precision: {}, recall : {}".format(iteration,l1,metric_,metric_1))
 else:
     weight_matrix = np.load('../../datasources/slim_W01.npz','csc')
-
-print("\n===SLIM ITEM-WEIGHT-SIMILARITY MATRIX===")
-print("Shape: {}".format(weight_matrix.shape))
-print("Non zero elements: {}".format(weight_matrix.getnnz()))
-print("Non zero elements all cols len: {}".format(len(weight_matrix.getnnz(axis=0))))
-print("Non zero elements all cols sum: {}".format(np.sum(weight_matrix.getnnz(axis=0))))
-print("Non zero elements all cols avg: {}".format(np.sum(weight_matrix.getnnz(axis=0))/len(weight_matrix.getnnz(axis=0))))
-
