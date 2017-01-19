3cb84446763b2e0478a0c7fb45517c07dddab225
LeonardoCella <leonardocella@gmail.com>
2017-01-19 15:01:18 +0100

    last updates

--- source/examples/slim_v01.py
+++ source/examples/slim_v01.py
@@ -18,7 +18,7 @@ percentage_sub_train_row = 0.7
 k = 5 # Number of k-items to make the evaluation on
 
 l1 = 0.1
-l2 = 1000
+l2 = 10
 
 #A) INIT
 # Dataset loading and partitioning
@@ -50,9 +50,11 @@ print("Original: {} , train : {} , test: {} ".format(netflix_urm.shape,train_URM
     # Compute the model with the established parameters
 model = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
 #model.fit(train_URMmatr
-
+#print("HOLDOUT")
+#trainURM, validationURM = holdout(train_URMmatrix, perc = 0.70, clean_test = True)
 
 if TO_COMPUTE_MATRIX:
+    print("INIT SIM MATRIX COMPUTATION")
     model.fit(train_URMmatrix)
     weight_matrix = model.get_weight_matrix()
     store_sparse_mat(weight_matrix,'../../datasorces/slimW_{}_{}.npz'.format(l1,l2))
@@ -77,6 +79,7 @@ if TO_COMPUTE_MATRIX:
     #print("Iteration {} , l1 - l2 coeff: {}@{}, precision: {}, recall : {}".format(iteration,l1,l2,metric_,metric_1))
 
 else:
+    print("INIT SIM MATRIX LOADING")
     weight_matrix = load_sparse_mat('../../datasources/slimW_{}_{}.npz'.format(l1,l2))
     model.set_urm_matrix(test_URMmatrix)
     model.set_weight_matrix(weight_matrix)
@@ -94,19 +97,24 @@ metric_ = 0.0
 metric_1 = 0.0
 iteration = 0
 
+#print(weight_matrix)
 
 users_test = np.unique(test_URMmatrix.nonzero()[0])
+#print(test_URMmatrix)
 print("\nITERATION FOR EVALUATION")
 for user_to_test in users_test:
     iteration += 1
-    print("Iteration: {} over {}".format(iteration, len(users_test)))
-    relevant_items = test_URMmatrix[user_to_test].indices
+    #print("Iteration: {} over {}".format(iteration, len(users_test)))
+    relevant_items = test_URMmatrix[user_to_test].nonzero()[1]
+    #print(test_URMmatrix[user_to_test])
     if len(relevant_items) > 0:
         n_eval += 1
         #print("SHAPES: train_URM {}, test_URM {}, weight {}, test_URM[u] {}".format(train_URMmatrix.shape, test_URMmatrix.shape, weight_matrix.shape, test_URMmatrix[user_to_test].shape))
-        recommended_items = model.recommend(user_id = user_to_test, exclude_seen=True)
+        recommended_items = model.recommend(user_id = user_to_test, exclude_seen=False)
+        #print("relevant items: {}\nrecommended items:{}\n".format(relevant_items, recommended_items))
         metric_ += precision(recommended_items, relevant_items, at=k)
         metric_1 += recall(recommended_items, relevant_items, at=k)
+        #print(metric_,metric_1)
 metric_ /= n_eval
 metric_1 /= n_eval
 print("The precision is {} and the recall: {}".format(metric_,metric_1))
