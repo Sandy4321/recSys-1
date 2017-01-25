aa900f37f9b922cc7fb72c59c38dac17decf6fb0
LeonardoCella <leonardocella@gmail.com>
2017-01-25 10:54:28 +0100

    renaming and init code reorganization

--- source/examples/main.py
+++ source/examples/main.py
@@ -2,7 +2,7 @@ from recSysLib.dataset_partition import DataPartition
 from recSysLib.data_split import holdout
 from recSysLib.data_utils import get_urm, load_sparse_mat, store_sparse_mat
 from recSysLib.metrics import precision, recall
-from recSysLib import slim
+from recSysLib import slim, 
 from recSysLib import abPredictor as abp
 
 import numpy as np
@@ -38,7 +38,8 @@ test_URMmatrix = urm_partition.get_lowLeft_matrix()
 
 #B) WEIGHT MATRIX COMPUTATION 
 print("Original: {} , train : {} , test: {} ".format(netflix_urm.shape,train_URMmatrix.shape, test_URMmatrix.shape))
-model = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
+model_slim = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
+model_cbf = content.Simple_CBF('Pearson')
 
 # TO CHECK
 #print("HOLDOUT")
@@ -47,8 +48,8 @@ model = slim.MultiThreadSLIM(l1_penalty=l1,l2_penalty=l2)
 
 if TO_COMPUTE_MATRIX:
     print("INIT SIM MATRIX COMPUTATION")
-    model.fit(train_URMmatrix)
-    weight_matrix = model.get_weight_matrix()
+    model_slim.fit(train_URMmatrix)
+    weight_matrix = model_slim.get_weight_matrix()
     store_sparse_mat(weight_matrix,BASELINE + 'slimW_{}_{}.npz'.format(l1,l2))
 
     #print("SLIM similarity nnz: ",len(weight_matrix.nonzero()[0]))
@@ -73,8 +74,8 @@ if TO_COMPUTE_MATRIX:
 else:
     print("INIT _ SIM MATRIX LOADING")
     weight_matrix = load_sparse_mat(BASELINE + 'slimW_{}_{}.npz'.format(l1,l2))
-    model.set_urm_matrix(test_URMmatrix)
-    model.set_weight_matrix(weight_matrix)
+    model_slim.set_urm_matrix(test_URMmatrix)
+    model_slim.set_weight_matrix(weight_matrix)
     #print(weight_matrix)
     if verbose > 0:
         print("Weight Matrix type: ",type(weight_matrix))
@@ -95,33 +96,32 @@ abPredictor = abp.abPredictor()
 
 # Compute residual and sampled matrices. (holdout for evaluation)
 print("DECOMPOSITION")
-model.decompose_urm_matrix(k, verbose = 0)
+model_slim.decompose_urm_matrix(k, verbose = 0)
 # Set of learning matrix as the sampled_csc
 print("PRE-EVAL")
-model.set_urm_matrix(model.get_residual_csc())
-model.set_weight_matrix(abPredictor.get_weight_matrix())
+model_slim.set_urm_matrix(model_slim.get_residual_csc())
+model_slim.set_weight_matrix(abPredictor.get_weight_matrix())
 #print(weight_matrix)
 
-users_test = np.unique(test_URMmatrix.nonzero()[0])
+
+##Helper function to do a single user
+def _do_user(user_to_test):
+    relevant_items = evaluation_URMmatrix[user_to_test].nonzero()[1] 
+    if len(relevant_items) > 0:
+        recommended_items = model_slim.recommend(user_id = user_to_test,
+                                                exclude_seen=True)
+        prec = precision(recommended_items, relevant_items, at=k)
+        rec = recall(recommended_items, relevant_items, at=k)
+        return (prec, rec)
+
+users_test = [1,2,3,4,5,6,7,8]#np.unique(test_URMmatrix.nonzero()[0])
 #print(test_URMmatrix)
 print("\nITERATION FOR EVALUATION")
-evaluation_URMmatrix = model.get_sampled_csc()
-for user_to_test in users_test:
-    iteration += 1
-    if iteration%100 == 0:
-        print("Iteration: {} over {}".format(iteration, len(users_test)))
-    relevant_items = evaluation_URMmatrix[user_to_test].nonzero()[1]
-    #print(test_URMmatrix[user_to_test])
-    if len(relevant_items) > 0:
-        n_eval += 1
-        #print("SHAPES: train_URM {}, test_URM {}, weight {}, test_URM[u] {}".format(train_URMmatrix.shape, test_URMmatrix.shape, weight_matrix.shape, test_URMmatrix[user_to_test].shape))
-        recommended_items = model.recommend(user_id = user_to_test,
-                                            exclude_seen=True)
-        print("relevant items: {}\nrecommended items: {}\nother items: {}\n\n".
-              format(relevant_items, recommended_items,model._get_user_ratings(user_to_test)))
-        metric_ += precision(recommended_items, relevant_items, at=k)
-        metric_1 += recall(recommended_items, relevant_items, at=k)
-        #print(metric_,metric_1)
-metric_ /= n_eval
-metric_1 /= n_eval
+evaluation_URMmatrix = model_slim.get_sampled_csc()
+import multiprocessing
+pool = multiprocessing.Pool(processes = multiprocessing.cpu_count())
+res = pool.map(_do_user, (u for u in users_test))
+
+print(res)
+
 print("The precision is {} and the recall: {}".format(metric_,metric_1))
--- source/recSysLib/slim.py
+++ source/recSysLib/slim.py
@@ -3,6 +3,10 @@ import scipy.sparse as sps
 from sklearn.linear_model import ElasticNet
 from .base import Recommender,check_matrix
 import random
+import pickle
+
+SAMPLED_CSC = "../../datasources/matrices/sampled.pkl"
+RESIDUAL_CSC = "../../datasources/matrices/residual.pkl"
 
 # extends the abstract class Recommender, it is contained in the file base.py
 class Slim(Recommender):
@@ -123,38 +127,54 @@ class Slim(Recommender):
         return self.residual_csc
 
     def decompose_urm_matrix(self, k, verbose = 0):
-        self.original_dataset = self.dataset
-        iteration = 0
-        list_users = np.unique(self.dataset.nonzero()[0])
-        csc_residual_dataset = sps.lil_matrix(self.dataset.shape)
-        csc_sampled_dataset = sps.lil_matrix(self.dataset.shape)
-        len_test = len(list_users)
-        for u in list_users:
-            iteration += 1
-            user_profile = self.dataset[u,:]
-            if verbose > 0 and iteration%500 == 0:
-                print("\nIteration {} over {}".format(iteration, len_test))
-                #print("Profile:",user_profile)
-            rated_items = list(user_profile.nonzero()[1])
-            if len(rated_items) > k:
-                sampled_items = random.sample(rated_items,k)
-                residual_items = list(set(rated_items) - set(sampled_items))
-                if verbose > 1:
-                    print("All items",rated_items)
-                    print("Sampled items type: {}, {}".format(type(sampled_items),sampled_items))
-                    print("Residual items ",residual_items)
-                for i in residual_items:
-                    csc_residual_dataset[u,i] = user_profile[0,i]
+        #try to load the matrices, otherwise compute and store them
+        try:
+            with open(SAMPLED_CSC, 'rb') as infile:
+                self.sampled_csc = pickle.load(infile)
+            with open(RESIDUAL_CSC, 'rb') as infile:
+                self.residual_csc = pickle.load(infile)
+            print("Loaded sampled and residual matrices")
+        except:
+            self.original_dataset = self.dataset
+            iteration = 0
+            list_users = np.unique(self.dataset.nonzero()[0])
+            csc_residual_dataset = sps.lil_matrix(self.dataset.shape)
+            csc_sampled_dataset = sps.lil_matrix(self.dataset.shape)
+            len_test = len(list_users)
+            for u in list_users:
+                iteration += 1
+                user_profile = self.dataset[u,:]
+                if verbose > 0 and iteration%500 == 0:
+                    print("\nIteration {} over {}".format(iteration, len_test))
+                    #print("Profile:",user_profile)
+                rated_items = list(user_profile.nonzero()[1])
+                if len(rated_items) > k:
+                    sampled_items = random.sample(rated_items,k)
+                    residual_items = list(set(rated_items) - set(sampled_items))
+                    if verbose > 1:
+                        print("All items",rated_items)
+                        print("Sampled items type: {}, {}".format(type(sampled_items),sampled_items))
+                        print("Residual items ",residual_items)
+                    for i in residual_items:
+                        csc_residual_dataset[u,i] = user_profile[0,i]
 
-                for i in sampled_items:
-                    csc_sampled_dataset[u,i] = user_profile[0,i]
-                    #print(csc_sampled_dataset[u,i])
-                #print("Sampled type:{}, {}".format(type(sampled_items), sampled_items))
-        if verbose > 0:
-            print("CSC sampled:\n",csc_sampled_dataset)
-            print("CSC residual:\n",csc_residual_dataset)
-        self.sampled_csc = csc_sampled_dataset
-        self.residual_csc = csc_residual_dataset
+                    for i in sampled_items:
+                        csc_sampled_dataset[u,i] = user_profile[0,i]
+                        #print(csc_sampled_dataset[u,i])
+                    #print("Sampled type:{}, {}".format(type(sampled_items), sampled_items))
+            if verbose > 0:
+                print("CSC sampled:\n",csc_sampled_dataset)
+                print("CSC residual:\n",csc_residual_dataset)
+            self.sampled_csc = csc_sampled_dataset
+            self.residual_csc = csc_residual_dataset
+
+            #and store them
+            with open(SAMPLED_CSC, 'wb') as outfile:
+                pickle.dump(self.sampled_csc, outfile, pickle.HIGHEST_PROTOCOL)
+            with open(RESIDUAL_CSC, 'wb') as outfile:
+                pickle.dump(self.residual_csc, outfile, pickle.HIGHEST_PROTOCOL)
+            print("Saved sampled and residual matrices")
+            print("Saved sampled and residual matrices")
 
 from multiprocessing import Pool
 from functools import partial
