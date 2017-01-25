0440697f52d59d4db35370d5b53eb99fb6b20692
LeonardoCella <leonardocella@gmail.com>
2017-01-25 13:33:51 +0100

    weight matrix CBF store and load

--- source/recSysLib/content.py
+++ source/recSysLib/content.py
@@ -1,8 +1,11 @@
 import numpy as np
 import scipy.sparse as sps
 import scipy.stats as stats
+import pickle
 from .base import Recommender,check_matrix
 
+WEIGHT_CBF = "../../datasources/matrices/cbf.pkl"
+MIN_SIM = 1e-5
 
 class Simple_CBF(Recommender):
     """
@@ -23,7 +26,7 @@ class Simple_CBF(Recommender):
     def fit(self, X, verbose = 0):
         self.icm = X
         X = check_matrix(X, format='csc', dtype=np.int32)
-
+        print("Init CBF weight matrix computation")
         if verbose > 0:
             print("ICM conversion to csc sparse matrix")
             print("ICM type: {}, shape: {}".format(type(X), X.shape))
@@ -34,22 +37,37 @@ class Simple_CBF(Recommender):
             print("Item Indexes {},\nlen: {}".format(item_indexes,
                                                      len(item_indexes)))
     
-        self.weight_matrix = sps.lil_matrix((len(item_indexes),len(item_indexes)), dtype=np.float32)
-        for i in item_indexes:
-            for j in item_indexes:
-                if i <= j:
-                    continue
-                if self.metric == 'Pearson':
-                    if verbose > 0:
-                        print("item i: {}\nitem j:{}".format(X[i],X[j]))
-                        print("content i:{}\ncontent j:{}".format(X[i].toarray()[0],X[j].toarray()[0]))
-                    c,_ = stats.pearsonr(X[i].toarray()[0], X[j].toarray()[0])
-                    if verbose > 0:
-                        print("item-1: {}\nitem-2:{}:sim{}".format(X[i].nonzero()[1],X[j].nonzero()[1], c))
-                self.weight_matrix[i,j] = c
-                self.weight_matrix[j,i] = c
-        if verbose > 0:
-            print("Final weight matrix: {}".format(self.weight_matrix))
+        try:
+            with open(WEIGHT_CBF, 'rb') as in_file:
+                self._weight_matrix = pickle.load(in_file)
+            print("Load CBF weight matrix")
+        except: 
+            self.weight_matrix = sps.lil_matrix((len(item_indexes),len(item_indexes)), dtype=np.float32)
+
+            for i in item_indexes:
+                if i%100==0:
+                    print("Iteration {} in CBF".format(i))
+                for j in item_indexes:
+                    if i <= j:
+                        continue
+                    if self.metric == 'Pearson':
+                        if verbose > 0:
+                            print("item i: {}\nitem j:{}".format(X[i],X[j]))
+                            print("content i:{}\ncontent j:{}".format(X[i].toarray()[0],X[j].toarray()[0]))
+                        c,_ = stats.pearsonr(X[i].toarray()[0], X[j].toarray()[0])
+                        if verbose > 0:
+                            print("item-1: {}\nitem-2:{}:sim{}".format(X[i].nonzero()[1],X[j].nonzero()[1], c))
+                        
+                    if c > MIN_SIM:
+                        self.weight_matrix[i,j] = c
+                        self.weight_matrix[j,i] = c
+            if verbose > 0:
+                print("Final weight matrix: {}".format(self.weight_matrix))
+            
+            with open(WEIGHT_CBF, 'wb') as out_file:
+                pickle.dump(self.weight_matrix, out_file, pickle.HIGHEST_PROTOCOL) 
+            
+            print("Weight computation - CBF, END") 
 
     def get_weight_matrix(self):
         return self.weight_matrix
